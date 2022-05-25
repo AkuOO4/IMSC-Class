20300a5c37a42c8428d081888154b36e8d2c30b3
AkuOO4 <95350596+AkuOO4@users.noreply.github.com>
2022-05-25 17:24:55 +0530

    Create reverse_of_a_number.java

---
+++ Java/reverse_of_a_number.java
@@ -0,0 +1,18 @@
+import java.util.*;  
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+	    Scanner sc= new Scanner(System.in);
+	    System.out.print("Enter first number- ");  
+        int a= sc.nextInt();  
+        int rev=0;
+	    do
+	    {
+	        rev=rev*10+a%10;
+	        a=a/10;
+	    }while(a!=0);
+	    System.out.print(rev);  
+	}
+}
