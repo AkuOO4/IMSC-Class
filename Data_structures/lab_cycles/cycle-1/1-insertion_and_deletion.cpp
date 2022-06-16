77dd14e64aa17b6df43d11ac7a55481da34bac86
AkuOO4 <akshayks2004@gmail.com>
2022-06-16 15:34:09 +0530

    good

--- Data_structures/lab_cycles/cycle-1/1-insertion_and_deletion.cpp
+++ Data_structures/lab_cycles/cycle-1/1-insertion_and_deletion.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 using namespace std;
 
-void insertion (int arr[], int element, int position,int len)
+int* insertion (int arr[],int len, int element, int position)
 {
-    int temp=arr[position-1];
-    int new_arr[len+1];
+    static  int *new_arr=new int(len+1);
     int i=0,k=0;
-    while (i<len+1)
+    while (i<=len+1)
     {
         if (i!=position)      
         {
@@ -15,22 +14,64 @@ void insertion (int arr[], int element, int position,int len)
         else
         {
             new_arr[i]=element;
-            k++;
+            //k++;
         }
         i++;
         k++;
     }
-    cout<<"array is\n";
-    for (int i=0;i<len+1;i++)
-        cout<<new_arr[i]<<" ";
-    cout<<endl;
+
+    return new_arr;
+}
+
+int* deletion(int arr[],int len,int element)
+{
+    static int *new_arr=new int(len-1);
+    int i=0,k=0;
+    while (k<len)
+    {
+        if (arr[k]!=element)      
+        {
+            new_arr[i]=arr[k];
+            i++;
+        }
+        
+        k++;
+        
+    }
+    return new_arr;
 }
 
 int main()
 {
     int arr[9]={1,2,3,4,5,6,7,8,9};
-    int ele=5;
-    int length=9;
-    int postion=5;
-    insertion(arr,ele,postion,length);
+    int ele;
+    int len=size(arr);
+
+    cout<<"1. Insertion\n2. Deletion\n";
+    int inp;
+    cin>>inp;
+
+    if (inp==1)
+    {
+        int postion;
+        cout<<"Enter the element and position to insert\n";
+        cin>>ele>>postion;
+        int *new_arr=new int(len+1);    
+        new_arr=insertion(arr,len,ele,postion);
+
+        for (int i=0;i<len;i++)
+            cout<<new_arr[i]<<endl;
+    }
+    else if(inp==2)
+    {
+        cout<<"Enter the element to delete\n";
+        cin>>ele;
+        int *new_arr=new int(len-1);
+        new_arr=deletion(arr,len,ele);
+        
+        for (int i=0;i<len-1;i++)
+            cout<<new_arr[i]<<endl;
+    }
+
+
 }
