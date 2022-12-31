fc310fdad85f44813c70a644ea06052c634f393c
AkuOO4 <akshayks2004@gmail.com>
2022-12-31 13:12:20 +0530

    kk

--- DBMS/sports_complex/src/main/java/SIGNUP.java
+++ DBMS/sports_complex/src/main/java/SIGNUP.java
@@ -1,3 +1,9 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javax.swing.*;
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
@@ -25,14 +31,15 @@ public class SIGNUP extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        popup = new javax.swing.JPopupMenu();
         jLabel1 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
+        username = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
-        jTextField2 = new javax.swing.JTextField();
+        psswrd = new javax.swing.JTextField();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
-        jTextField3 = new javax.swing.JTextField();
-        jButton1 = new javax.swing.JButton();
+        confrmpsswrd = new javax.swing.JTextField();
+        signup = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -40,9 +47,9 @@ public class SIGNUP extends javax.swing.JFrame {
 
         jLabel2.setText("Password");
 
-        jTextField2.addActionListener(new java.awt.event.ActionListener() {
+        psswrd.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField2ActionPerformed(evt);
+                psswrdActionPerformed(evt);
             }
         });
 
@@ -51,7 +58,12 @@ public class SIGNUP extends javax.swing.JFrame {
 
         jLabel4.setText("Confirm Password");
 
-        jButton1.setText("SIGN UP");
+        signup.setText("SIGN UP");
+        signup.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                signupActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -65,15 +77,15 @@ public class SIGNUP extends javax.swing.JFrame {
                     .addComponent(jLabel4))
                 .addGap(74, 74, 74)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jTextField2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 106, Short.MAX_VALUE)
-                    .addComponent(jTextField3)
-                    .addComponent(jTextField1))
+                    .addComponent(psswrd, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 106, Short.MAX_VALUE)
+                    .addComponent(confrmpsswrd)
+                    .addComponent(username))
                 .addGap(97, 97, 97))
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addGap(171, 171, 171)
-                        .addComponent(jButton1))
+                        .addComponent(signup))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(110, 110, 110)
                         .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 159, javax.swing.GroupLayout.PREFERRED_SIZE)))
@@ -87,26 +99,77 @@ public class SIGNUP extends javax.swing.JFrame {
                 .addGap(30, 30, 30)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(username, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(31, 31, 31)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2)
-                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(psswrd, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(32, 32, 32)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel4)
-                    .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(confrmpsswrd, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 48, Short.MAX_VALUE)
-                .addComponent(jButton1)
+                .addComponent(signup)
                 .addGap(16, 16, 16))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jTextField2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField2ActionPerformed
+    private void psswrdActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_psswrdActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField2ActionPerformed
+    }//GEN-LAST:event_psswrdActionPerformed
+
+    private void signupActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_signupActionPerformed
+        // TODO add your handling code here:
+        String Username= username.getText();
+        String password= psswrd.getText();
+        String confirm_password= confrmpsswrd.getText();
+    if (password.equalsIgnoreCase(confirm_password)){
+        
+        try{
+      Class.forName("com.mysql.cj.jdbc.Driver");
+      Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3307/sports_complex","root","root");
+      Statement stmt=conn.createStatement();
+      
+      
+      
+      
+      String q2="select max(cust_id) from costumer_details";
+      ResultSet rs1=stmt.executeQuery(q2);
+      rs1.next();
+      int id=rs1.getInt(1)+1;
+      
+      String q1="insert into costumer_details values ('"+id+"','"+Username+"','"+password+"');";
+      //System.out.print(q1);
+      
+      //String[] data;
+      stmt.executeUpdate(q1);
+      JOptionPane.showMessageDialog(null, "Account succesfully created","succes", JOptionPane.OK_OPTION);
+      
+      start_page jf1 =new start_page();
+      jf1.show();
+      dispose();
+      
+      //ResultSet rs2=stmt.executeUpdate(q2);
+      //System.out.println("123");
+      
+      //removeAllItems();
+      //int count=0;
+     
+      //return data;
+    } 
+    catch (Exception ex) {
+      System.out.print("\n--ERROR--\n");
+      System.out.print(ex.getMessage());
+      JOptionPane.showMessageDialog(null, ex.getMessage(),"ERROR", JOptionPane.ERROR_MESSAGE);
+
+    }
+        }
+    else{
+        JOptionPane.showMessageDialog(null, "Password and confirmation password are different.","ERROR", JOptionPane.ERROR_MESSAGE);
+    }
+    }//GEN-LAST:event_signupActionPerformed
 
     /**
      * @param args the command line arguments
@@ -144,13 +207,14 @@ public class SIGNUP extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
+    private javax.swing.JTextField confrmpsswrd;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
+    private javax.swing.JPopupMenu popup;
+    private javax.swing.JTextField psswrd;
+    private javax.swing.JButton signup;
+    private javax.swing.JTextField username;
     // End of variables declaration//GEN-END:variables
 }
